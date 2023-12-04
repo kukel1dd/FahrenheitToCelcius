caee808e8065b336c199fbeb7718a95ae894be66
Luke <133702477+kukel1dd@users.noreply.github.com>
2023-12-04 14:13:48 -0500

    Add files via upload

---
+++ Convert_temp_withLogic/Convert_temp_withLogic/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+    'This project takes the users Input of fahrenheit and converts it to celsius.
+
+    Sub Main()
+        Dim dblF, dblC As Double
+        dblF = Get_Data()
+        dblC = CalcCelc(dblF)
+        DisplayResults(dblF, dblC)
+    End Sub
+
+    Function Get_Data() As Double
+
+        Dim dblFGet As Double
+        dblFGet = Convert.ToDouble(InputBox("Enter The Fahrenheit: "))
+        Return dblFGet
+    End Function
+
+    Function CalcCelc(ByVal DoubleFTemp As Double) As Double
+        Dim dblCtemp As Double
+        dblCtemp = (DoubleFTemp - 32) * (5 / 9)
+        Return dblCtemp
+    End Function
+
+    Sub DisplayResults(ByVal DoubleF As Double, ByVal DoubleC As Double)
+        Console.WriteLine("Fahrenheit: " & DoubleF & " Celsius: " & DoubleC)
+    End Sub
+
+
+
+
+
+
+
+End Module
---
+++ Convert_temp_withLogic/Convert_temp_withLogic/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Convert_temp_withLogic")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("Convert_temp_withLogic")>
+<Assembly: AssemblyCopyright("Copyright © HP 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("880eb082-415c-453f-b6e4-2e409268182d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
